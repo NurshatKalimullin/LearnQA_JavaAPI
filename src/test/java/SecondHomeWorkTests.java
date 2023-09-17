4264c18f83b5857dccb71837a028f6d3473978f0
Nurshat Kalimullin <nurshatkalimullin@mail.ru>
2023-09-17 14:39:01 +0300

    Print Location header

--- src/test/java/SecondHomeWorkTests.java
+++ src/test/java/SecondHomeWorkTests.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -17,5 +18,20 @@ public class SecondHomeWorkTests {
                 response
                         .get("messages[1].message");
         System.out.println(second);
+    }
+
+
+    @Test
+    public void testRedirect() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
     }
 }
